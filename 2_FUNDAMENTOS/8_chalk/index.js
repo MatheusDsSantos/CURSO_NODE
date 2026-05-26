a19e594dda5f0d6da75c483688754ada9a27c858
Matheus Santos <matheushsantos17@gmail.com>
2026-05-26 13:59:53 -0300

    chalk

---
+++ 2_FUNDAMENTOS/8_chalk/index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk')
+
+const nota = 4
+
+
+if(nota >= 7){
+    console.log(chalk.green('parabens voce esta aprovado!'))
+}else{
+    console.log(chalk.bgRed.black('precisa fazer a prova de recuperação!'))
+}
