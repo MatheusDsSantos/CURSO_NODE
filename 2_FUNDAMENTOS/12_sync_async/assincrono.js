5a808210da9af5e5923557f9307a7e3224073b03
Matheus Santos <matheushsantos17@gmail.com>
2026-05-26 16:37:30 -0300

    sincrono e assincrono exemplos

---
+++ 2_FUNDAMENTOS/12_sync_async/assincrono.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+console.log('inicio')
+
+fs.writeFile("arquivo.txt", 'oi', function(err){
+    setTimeout(() => {
+        console.log('arquivo criado!')
+    }, 2000);
+}
+
+)
+
+console.log('fim')
