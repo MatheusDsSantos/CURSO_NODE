7fd338812351801d03512ac5ddb7656a65c781db
Matheus Santos <matheushsantos17@gmail.com>
2026-05-26 10:59:19 -0300

    atividade01_cursoNode

---
+++ modulo_01/projeto.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+let soma = a + b;
+
+console.log(soma);
+
+//comandos usados no terminal: npm init -y e npm install
+//usaria esses comandos pra poder usar o nome diretamente do projeto.js
