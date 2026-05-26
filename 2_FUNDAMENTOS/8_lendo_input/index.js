e28fdea521e0b33603ea81e706a57201afda24c8
Matheus Santos <matheushsantos17@gmail.com>
2026-05-26 14:06:58 -0300

    lendo input do user

---
+++ 2_FUNDAMENTOS/8_lendo_input/index.js
@@ -0,0 +1,15 @@
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+
+})
+
+
+readline.question("qual a sua linguagem preferida?", (language)=>{
+    if(language === "python"){
+    console.log(`isso nem é linguagem man`);
+    }else{
+        console.log(`a minha linguagem preferida é ${language}`)
+    }
+    readline.close();
+})
