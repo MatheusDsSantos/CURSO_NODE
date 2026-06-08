fb260952dad62a7935f182749ae9fee69a7eddb0
Matheus Santos <matheushsantos17@gmail.com>
2026-06-08 08:35:41 -0300

    routes

---
+++ 6_EXPRESS/8_modulo_rotas/users/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const basePath = path.join(__dirname,'../templates')
+
+
+
+router.get('/:id', (req,res)=>{
+    const id = req.params.id
+    //leitura da tabela users, resgatar um usuario do banco
+    console.log(`estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+module.exports = router
